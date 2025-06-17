3d994fe291fafae0b75e78e2446cfd25bb8b379d
Abdelrahman <nar7gamer@gmail.com>
2025-06-17 11:08:03 +0800

    alright i am 90% done

--- src/index.php
+++ src/index.php
@@ -25,6 +25,7 @@
         <li><a href="#about">About Us</a></li>
         <li><a href="#team-members">Meet the Team</a></li>
         <li><a href="#projects">Projects</a></li>
+        <li><a href="project.php">Project Showcase</a></li>
         <li><a href="#gallery">Gallery</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
